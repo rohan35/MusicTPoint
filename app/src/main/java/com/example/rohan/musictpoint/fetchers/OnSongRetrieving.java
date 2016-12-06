b2cf39caec7316b9323ed85a3f8293e4e818f91c
Rohan Sharma <rohansharma2839@gmail.com>
2016-12-06 13:31:19 +0530

    Update OnSongRetrieving.java

--- app/src/main/java/com/example/rohan/musictpoint/fetchers/OnSongRetrieving.java
+++ app/src/main/java/com/example/rohan/musictpoint/fetchers/OnSongRetrieving.java
@@ -4,9 +4,6 @@ import com.example.rohan.musictpoint.model.Song;
 
 import java.util.ArrayList;
 
-/**
- * Created by anirudh.r on 04/12/16.
- */
 
 public interface OnSongRetrieving {
     void retrievedSong(ArrayList<Song> songArrayList);
